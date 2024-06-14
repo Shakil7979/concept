e3f041182c10d829707985ea343faa4190552d94
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-06-14 20:54:09 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,18 @@
 $(document).ready(function(){
-	// Mobile Menu
-	// $('.logo a i').click(function(){
-	// 	$('.menu ul').slideToggle(1000);
-
-	// 	return false
-	// });
-
-	// Banner Carousel
-	// $('.banner_carousel').owlCarousel({
-	// 	items:1,
-	// 	loop:true,
-	// 	nav:true,
-	// 	dots:true,
-	// 	autoplay:true,
-	// });
 
 
+	$(document).on('click','.right_content_item',function(){ 
+		$('.right_hide_content').hide();
+		$(this).find('.right_hide_content').show();
+	})
+
+	$(document).on('click','.search_order',function(){ 
+		$('.table_search_slide_box').css({'right':'0'});   
+	})
+
+	$(document).on('click','.btn_arrow_search',function(){ 
+		$('.table_search_slide_box').css({'right':'-100%'});   
+	})
 
 
 
