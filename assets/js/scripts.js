f78239001c2a69b8cc812fcd0ec064592ef2c32c
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-06-16 00:11:38 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,15 +4,29 @@ $(document).ready(function(){
 	$(document).on('click','.right_content_item',function(){ 
 		$('.right_hide_content').hide();
 		$(this).find('.right_hide_content').show();
-	})
+	});
 
 	$(document).on('click','.search_order',function(){ 
 		$('.table_search_slide_box').css({'right':'0'});   
-	})
+	});
 
 	$(document).on('click','.btn_arrow_search',function(){ 
 		$('.table_search_slide_box').css({'right':'-100%'});   
-	})
+	});
+
+	$(document).on('click','.card_add_more',function(){ 
+		$('.cart_product_popup').css({'bottom':'0'});   
+	});
+	
+	$(document).on('click','.for_for_card',function(){ 
+		$('.order_calculate_popup').css({'right':'0'});   
+
+		return false;
+	});
+
+	$(document).on('click','.btn_arrow_search_cart',function(){ 
+		$('.order_calculate_popup').css({'right':'-100%'});   
+	});
 
 
 
